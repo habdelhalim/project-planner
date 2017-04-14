fe1ab09a345a19119258b49c5874d4bdb0fa5b03
Hasan Abdel Halim <hasana@gmail.com>
2017-04-14 23:25:38 +0300

    initial project import

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projectPlannerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the projectPlannerApp
+ */
+angular.module('projectPlannerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
