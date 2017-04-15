ba0fece9b5cf30f5614ff81b06ed849c6df40852
Hasan Abdel Halim <hasana@gmail.com>
2017-04-15 22:41:08 +0300

    added time frames and date

--- app/scripts/directives/planner.js
+++ app/scripts/directives/planner.js
@@ -13,6 +13,39 @@ angular.module('projectPlannerApp')
       scope: {
         'data': '='
       },
-      restrict: 'E'
+      restrict: 'E',
+      controller: function () {
+        var vm = this;
+
+        vm.timeFrames = {
+          day: {
+            start: moment('9:00', 'HH:mm'),
+            end: moment('18:00', 'HH:mm'),
+            working: true, // This is a working period
+            default: true // It will be used for each day
+          },
+          noon: {
+            start: moment('12:00', 'HH:mm'),
+            end: moment('13:00', 'HH:mm'),
+            magnet: false, // This will disable magnet snapping
+            working: false, // This is a resting period
+            default: true // It will be used for each day
+          },
+          closed: {
+            magnet: false, // This will disable magnet snapping
+            working: false // We don't work when it's closed
+          }
+        };
+
+        vm.dateFrames = {
+          weekend: {
+            evaluator: function (date) { // A custom function evaluated for each day in the gantt
+              return date.isoWeekday() === 5 || date.isoWeekday() === 6;
+            },
+            targets: ['closed'] // Use timeFrame named closed for saturday and sunday.
+          }
+        };
+      },
+      controllerAs: 'vm'
     };
   });
